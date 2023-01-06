1adb4affa9a91a7783c576f45c231436af39a9c7
pm <peter.melichar@fjd.de>
2023-01-06 17:39:52 +0100

    Added script chkdestination.py, modified read_config.py to read destination.yaml and enabled environments STAGING and PRODUCTION in fitconnect.py to make chkdestination.py working Addes some Comments to source files and mentioned chkdestination.py in README.md.

--- read_config.py
+++ read_config.py
@@ -1,9 +1,11 @@
 from fitconnect import Environment
 from os import path
-from strictyaml import load, Map, Str, Int, Seq, YAMLError
+from strictyaml import load, Map, Str, Int, Seq, YAMLError, Optional
 from strictyaml import Enum as YAMLEnum
 import json
 
+'''Read data for a sender client from the specified configuration file,
+see the sender.yaml.example for required details.'''
 def read_config_sender(config_file):
     config_schema = Map({
         "destination_id": Str(),
@@ -20,6 +22,8 @@ def read_config_sender(config_file):
         config = load(file.read(), config_schema, label=config_file).data
     return config
 
+'''Read data for a subscriber client from the specified configuration file
+and its private key, see the subscriber.yaml.example for required details.'''
 def read_config_subscriber(config_file):
     config_schema = Map({
         "destination_id": Str(),
@@ -39,4 +43,34 @@ def read_config_subscriber(config_file):
     with open(path.join(path.dirname(config_file), config['private_key_decryption_file'])) as private_key_file:
         config['private_key_decryption'] = json.load(private_key_file)
 
+    return config
+
+'''Read data for sender clients to verify if a destination exists. At least one sender
+client configuration is required. For specific deteils see the destination.yaml.example'''
+def read_config_destination(config_file):
+    config_schema = Map({
+        Optional("TESTING"): Map({
+            # change to native Enum when strictyaml supports it: https://github.com/crdoconnor/strictyaml/issues/73
+            "environment": YAMLEnum([e.name for e in Environment]),
+            "client_id": Str(),
+            "client_secret": Str(),
+        }),
+        Optional("STAGING"): Map({
+            # change to native Enum when strictyaml supports it: https://github.com/crdoconnor/strictyaml/issues/73
+            "environment": YAMLEnum([e.name for e in Environment]),
+            "client_id": Str(),
+            "client_secret": Str(),
+        }),
+        Optional("PRODUCTION"): Map({
+            # change to native Enum when strictyaml supports it: https://github.com/crdoconnor/strictyaml/issues/73
+            "environment": YAMLEnum([e.name for e in Environment]),
+            "client_id": Str(),
+            "client_secret": Str(),
+        }),
+
+    })
+
+    # parse yaml config
+    with open(config_file) as file:
+        config = load(file.read(), config_schema, label=config_file).data
     return config
